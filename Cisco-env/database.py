788b51cb8b2e389223a1b3fafd124784dd2ba8e8
Jdeww <jdwilaysono@gmail.com>
2026-01-29 17:36:31 -0800

    Added simple database connection

---
+++ Cisco-env/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext import declarative_base
+
+URL_DATABASE = 'mysql+pymysql://'
+
+engine = create_engine(URL_DATABASE)
+
+SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
+
+Base = declarative_base()
